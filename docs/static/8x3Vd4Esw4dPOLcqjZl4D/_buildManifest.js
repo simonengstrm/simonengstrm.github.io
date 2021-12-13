d57b626ffb52ca0eb34d5638a79311b6029aed2d
LilleLink <simon_0_0_@live.se>
2021-12-13 01:28:56 +0100

    Changes build directory for next

---
+++ docs/static/8x3Vd4Esw4dPOLcqjZl4D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1ba38d192c0351c9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
