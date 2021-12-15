c529751cfeb07cadcf0e71951632328ff8cfc541
LilleLink <simon_0_0_@live.se>
2021-12-15 17:12:09 +0100

    Design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import React, {useState} from 'react';
 import github from '../public/github.png';
 import linkedin from '../public/linkedin.png';
+import profile from '../public/profile.jpeg'
 
 function Header() {
 
     return (
         <div className="mt-10 ml-10 mr-10 mb-5">
-            <p className="text-6xl tracking-wide font-bold">SIMON ENGSTRÖM</p>
-            <Links/>
+            <div className="flex gap-x-4 grid-cols-1 md:grid-cols-2 md:auto-cols-min md:items-center">
+                <div className="min-w-fit">
+                    <img src={profile.src} className="w-48 rounded-full "/>
+                </div>
+                <div className="flex flex-col">
+                    <div>
+                        <p className="text-6xl tracking-wide font-bold">SIMON ENGSTRÖM</p>
+                    </div>
+                    <div>
+                        <Links/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
@@ -31,36 +43,17 @@ function Content() {
     }
 
     return (
-        <div className="grid place-content-right ml-10 mr-10">
-            <p className="font-bold text-3xl">About me</p>
-            <p className="font-serif">I am a {calcAge()} year-old student currently residing in Gothenburg, Sweden, 
-            where I study Software Engineering at Chalmers University of Technology. I am a hard working, social and open person.
-            In my spare time I enjoy developing hobby projects, listening to music and socializing.</p> 
+        <div className="grid place-content-right ml-10 mr-10 gap-y-5">
+            <div>
+                <p className="font-bold text-3xl">About me</p>
+                <p className="font-serif">I am a {calcAge()} year-old student currently residing in Gothenburg, Sweden,
+                where I study Software Engineering at Chalmers University of Technology. I am a hard working, social and open person.
+                In my spare time I enjoy developing hobby projects, listening to music and socializing.</p>
+            </div> 
 
-            <div className="grid grid-cols-1 overflow-hidden md:divide-slate-400 md:grid-cols-2 md:gap-x-4">
-                <div className="my-3 space-y-4">
-                    <p className="font-bold text-3xl">School</p>
-                    <div>
-                        <p className="text-2xl">Chalmers University of Technology</p>
-                        <p className="italic text-slate-400">2020 → now</p>
-                        <p className="font-serif">I am currently studying my second year at Chalmers University of Technology, pursuing a bachelors degree,
-                        then a masters degree in Software Engineering. I am part of the <a href="https://digit.chalmers.it">digIT 21</a> committeé, 
-                        which develops and maintains the many web services of the <a href="https://chalmers.it/">IT-division</a>.</p>
-                    </div>
-
-                    <div>
-                        <p className="text-2xl">NTI-Gymnasiet Skövde</p>
-                        <p className="italic text-slate-400">2016 → 2019</p>
-                        <p className="font-serif">I studied information technology with software development specialization. 
-                        During the third year I was a student ambassador for the school.
-                        When graduating, I was awarded with best <a href="https://github.com/LilleLink/Gravity">graduation project</a>, 
-                        best grades, and became valedictorian. 
-                        </p>
-                    </div>
-
-                </div>
-                <div className="my-3 space-y-4">
-                    <p className="font-bold text-3xl">Work</p>
+            <div className="grid grid-cols-1 gap-y-10 overflow-hidden md:divide-slate-400 md:grid-cols-2 md:gap-x-10">
+                <div className="space-y-8">
+                    <p className="font-bold text-3xl border-b-2">Work</p>
 
                     <div>
                         <p className="text-2xl">Dialect IT Solutions</p>
@@ -88,6 +81,27 @@ function Content() {
                     </div>
 
                 </div>
+                <div className="space-y-8">
+                    <p className="font-bold text-3xl border-b-2">School</p>
+                    <div>
+                        <p className="text-2xl">Chalmers University of Technology</p>
+                        <p className="italic text-slate-400">2020 → now</p>
+                        <p className="font-serif">I am currently studying my second year at Chalmers University of Technology, pursuing a bachelors degree,
+                        then a masters degree in Software Engineering. I am part of the <a href="https://digit.chalmers.it">digIT 21</a> committeé, 
+                        which develops and maintains the many web services of the <a href="https://chalmers.it/">IT-division</a>.</p>
+                    </div>
+
+                    <div>
+                        <p className="text-2xl">NTI-Gymnasiet Skövde</p>
+                        <p className="italic text-slate-400">2016 → 2019</p>
+                        <p className="font-serif">I studied information technology with software development specialization. 
+                        During the third year I was a student ambassador for the school.
+                        When graduating, I was awarded with best <a href="https://github.com/LilleLink/Gravity">graduation project</a>, 
+                        best grades, and became valedictorian. 
+                        </p>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
@@ -96,7 +110,7 @@ function Content() {
 export default function Home() {
 
     return (
-        <div>
+        <div className="md:mx-40 mb-10">
             <Header/>
             <Content/>
         </div>
