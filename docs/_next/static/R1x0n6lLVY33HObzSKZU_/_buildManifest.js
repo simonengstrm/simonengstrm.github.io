dcaf3a72733bedaa61c4841c9238b609ed530e4b
LilleLink <simon_0_0_@live.se>
2022-10-03 15:37:46 +0200

    Forgot to build

---
+++ docs/_next/static/R1x0n6lLVY33HObzSKZU_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-84dc1291e96d1538.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
